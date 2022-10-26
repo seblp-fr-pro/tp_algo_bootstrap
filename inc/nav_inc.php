85660bcfaea07f8e2ba3d85c84d25953d8be794f
Lesort-Pajot Sebastien <sebastien@lesort-pajot.com>
2022-10-26 23:35:59 +0000

    2022-10-26--23-35

---
+++ inc/nav_inc.php
@@ -0,0 +1,40 @@
+<ul class="nav nav-tabs">
+<li class="active"><a data-toggle="tab" href="#menu1">Menu1</a></li>
+<li><a data-toggle="tab" href="#menu2">Menu2</a></li>
+<li><a data-toggle="tab" href="#menu3">Menu3</a></li>
+</ul>
+<div class="tab-content">
+<div id="menu1" class="tab-pane fade in active"> 
+<h3>Menu1</h3>
+Create your own dynamic tab menu using the bootstrap nav tabs and open dynamic content on click of menus.
+</div>
+<div id="menu2" class="tab-pane fade"> 
+<h3>Menu2</h3>
+Second tab menu. 
+</div>
+<div id="menu3" class="tab-pane fade"> 
+<h3>Menu3</h3>	
+Third tab menu.	
+</div>
+</div>
+
+	
+<ul class="nav nav-tabs">
+	<li class="active"><a data-toggle="tab" href="#menu1">Menu1</a></li>
+    <li><a data-toggle="tab" href="#menu2">Menu2</a></li>
+    <li><a data-toggle="tab" href="#menu3">Menu3</a></li>
+</ul>
+<div class="tab-content">
+	 <div id="menu1" class="tab-pane fade in active"> 
+     	<h3>Menu1</h3>
+     	Create your own dynamic tab menu using the bootstrap nav tabs and open dynamic content on click of menus.
+     </div>
+     <div id="menu2" class="tab-pane fade"> 
+     	<h3>Menu2</h3>
+     	Second tab menu. 
+     </div>
+     <div id="menu3" class="tab-pane fade"> 
+     	<h3>Menu3</h3>	
+     	Third tab menu.	
+     </div>
+</div>
