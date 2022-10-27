fc6643ddbe8027e84b89d6ae6c7b37056e93034e
Lesort-Pajot Sebastien <sebastien@lesort-pajot.com>
2022-10-27 13:57:35 +0000

    2022-10-27--13-57

--- inc/nav_inc.php
+++ inc/nav_inc.php
@@ -1,13 +1,175 @@
-<ul class="nav nav-tabs">
-    <li class="active"><a data-toggle="tab" href="#s01">Saison 01</a></li>
-    <li><a data-toggle="tab" href="#s02">Saison 02</a></li>
-    <li><a data-toggle="tab" href="#s03">Saison 03</a></li>
-    <li><a data-toggle="tab" href="#s04">Saison 04</a></li>
-    <li><a data-toggle="tab" href="#s05">Saison 05</a></li>
-    <li><a data-toggle="tab" href="#s06">Saison 06</a></li>
-    <li><a data-toggle="tab" href="#s07">Saison 07</a></li>
-    <li><a data-toggle="tab" href="#s08">Saison 08</a></li>
-    <li><a data-toggle="tab" href="#s09">Saison 09</a></li>
-    <li><a data-toggle="tab" href="#s10">Saison 10</a></li>
-    <li><a data-toggle="tab" href="#sCRUD">Saison CRUD</a></li>
-</ul>
+<!-- Saison 01 -->
+<div class="btn-group">
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+    Saison 01</button>
+    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <li><a class="dropdown-item" href="#s01">Exercice 01</a></li>
+        <li><a class="dropdown-item" href="#s02">Exercice 02</a></li>
+        <li><a class="dropdown-item" href="#s03">Exercice 03</a></li>
+        <li><a class="dropdown-item" href="#s04">Exercice 04</a></li>
+        <li><a class="dropdown-item" href="#s05">Exercice 05</a></li>
+        <li><a class="dropdown-item" href="#s06">Exercice 06</a></li>
+        <li><a class="dropdown-item" href="#s07">Exercice 07</a></li>
+        <li><a class="dropdown-item" href="#s08">Exercice 08</a></li>
+        <li><a class="dropdown-item" href="#s09">Exercice 09</a></li>
+    </ul>
+</div>
+
+<!-- Saison 02 -->
+<div class="btn-group">
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+    Saison 02</button>
+    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <li><a class="dropdown-item" href="#s01">Exercice 01</a></li>
+        <li><a class="dropdown-item" href="#s02">Exercice 02</a></li>
+        <li><a class="dropdown-item" href="#s03">Exercice 03</a></li>
+        <li><a class="dropdown-item" href="#s04">Exercice 04</a></li>
+    </ul>
+</div>
+
+<!-- Saison 03 -->
+<div class="btn-group">
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+    Saison 03</button>
+    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <li><a class="dropdown-item" href="#s01">Exercice 01</a></li>
+        <li><a class="dropdown-item" href="#s02">Exercice 02</a></li>
+        <li><a class="dropdown-item" href="#s03">Exercice 03</a></li>
+        <li><a class="dropdown-item" href="#s04">Exercice 04</a></li>
+        <li><a class="dropdown-item" href="#s05">Exercice 05</a></li>
+        <li><a class="dropdown-item" href="#s06">Exercice 06</a></li>
+    </ul>
+</div>
+
+<!-- Saison 04 -->
+<div class="btn-group">
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+    Saison 04</button>
+    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <li><a class="dropdown-item" href="#s01">Exercice 01</a></li>
+        <li><a class="dropdown-item" href="#s02">Exercice 02</a></li>
+        <li><a class="dropdown-item" href="#s03">Exercice 03</a></li>
+        <li><a class="dropdown-item" href="#s04">Exercice 04</a></li>
+        <li><a class="dropdown-item" href="#s05">Exercice 05</a></li>
+        <li><a class="dropdown-item" href="#s06">Exercice 06</a></li>
+        <li><a class="dropdown-item" href="#s07">Exercice 07</a></li>
+        <li><a class="dropdown-item" href="#s08">Exercice 08</a></li>
+    </ul>
+</div>
+
+<!-- Saison 05 -->
+<div class="btn-group">
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+    Saison 05</button>
+    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <li><a class="dropdown-item" href="#s01">Exercice 01</a></li>
+        <li><a class="dropdown-item" href="#s02">Exercice 02</a></li>
+        <li><a class="dropdown-item" href="#s03">Exercice 03</a></li>
+        <li><a class="dropdown-item" href="#s04">Exercice 04</a></li>
+        <li><a class="dropdown-item" href="#s05">Exercice 05</a></li>
+        <li><a class="dropdown-item" href="#s06">Exercice 06</a></li>
+        <li><a class="dropdown-item" href="#s07">Exercice 07</a></li>
+        <li><a class="dropdown-item" href="#s08">Exercice 08</a></li>
+        <li><a class="dropdown-item" href="#s09">Exercice 09</a></li>
+        <li><a class="dropdown-item" href="#s10">Exercice 10</a></li>
+        <li><a class="dropdown-item" href="#s10">Exercice 11</a></li>
+    </ul>
+</div>
+
+<!-- Saison 06 -->
+<div class="btn-group">
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+    Saison 06</button>
+    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <li><a class="dropdown-item" href="#s01">Exercice 01</a></li>
+        <li><a class="dropdown-item" href="#s02">Exercice 02</a></li>
+        <li><a class="dropdown-item" href="#s03">Exercice 03</a></li>
+        <li><a class="dropdown-item" href="#s04">Exercice 04</a></li>
+        <li><a class="dropdown-item" href="#s05">Exercice 05</a></li>
+        <li><a class="dropdown-item" href="#s06">Exercice 06</a></li>
+        <li><a class="dropdown-item" href="#s07">Exercice 07</a></li>
+        <li><a class="dropdown-item" href="#s08">Exercice 08</a></li>
+        <li><a class="dropdown-item" href="#s09">Exercice 09</a></li>
+        <li><a class="dropdown-item" href="#s10">Exercice 10</a></li>
+        <li><a class="dropdown-item" href="#s10">Exercice 11</a></li>
+        <li><a class="dropdown-item" href="#s10">Exercice 12</a></li>
+        <li><a class="dropdown-item" href="#s10">Exercice 13</a></li>
+        <li><a class="dropdown-item" href="#s10">Exercice 14</a></li>
+        <li><a class="dropdown-item" href="#s10">Exercice 15</a></li>
+        <li><a class="dropdown-item" href="#s10">Exercice 16</a></li>
+        <li><a class="dropdown-item" href="#s10">Exercice 17</a></li>
+    </ul>
+</div>
+
+<!-- Saison 07 -->
+<div class="btn-group">
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+    Saison 07</button>
+    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <li><a class="dropdown-item" href="#s01">Exercice 01</a></li>
+        <li><a class="dropdown-item" href="#s02">Exercice 02</a></li>
+        <li><a class="dropdown-item" href="#s03">Exercice 03</a></li>
+        <li><a class="dropdown-item" href="#s04">Exercice 04</a></li>
+        <li><a class="dropdown-item" href="#s05">Exercice 05</a></li>
+        <li><a class="dropdown-item" href="#s06">Exercice 06</a></li>
+        <li><a class="dropdown-item" href="#s07">Exercice 07</a></li>
+    </ul>
+</div>
+
+<!-- Saison 08 -->
+<div class="btn-group">
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+    Saison 08</button>
+    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <li><a class="dropdown-item" href="#s01">Exercice 01</a></li>
+        <li><a class="dropdown-item" href="#s02">Exercice 02</a></li>
+        <li><a class="dropdown-item" href="#s03">Exercice 03</a></li>
+        <li><a class="dropdown-item" href="#s04">Exercice 04</a></li>
+        <li><a class="dropdown-item" href="#s05">Exercice 05</a></li>
+        <li><a class="dropdown-item" href="#s06">Exercice 06</a></li>
+        <li><a class="dropdown-item" href="#s07">Exercice 07</a></li>
+    </ul>
+</div>
+
+<!-- Saison 09 -->
+<div class="btn-group">
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+    Saison 09</button>
+    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <li><a class="dropdown-item" href="#s01">Exercice 01</a></li>
+        <li><a class="dropdown-item" href="#s02">Exercice 02</a></li>
+        <li><a class="dropdown-item" href="#s03">Exercice 03</a></li>
+        <li><a class="dropdown-item" href="#s04">Exercice 04</a></li>
+        <li><a class="dropdown-item" href="#s05">Exercice 05</a></li>
+        <li><a class="dropdown-item" href="#s06">Exercice 06</a></li>
+        <li><a class="dropdown-item" href="#s07">Exercice 07</a></li>
+        <li><a class="dropdown-item" href="#s08">Exercice 08</a></li>
+        <li><a class="dropdown-item" href="#s09">Exercice 09</a></li>
+    </ul>
+</div>
+
+<!-- Saison 10 -->
+<div class="btn-group">
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+    Saison 10<button>
+    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <li><a class="dropdown-item" href="#s01">Exercice 01</a></li>
+        <li><a class="dropdown-item" href="#s02">Exercice 02</a></li>
+        <li><a class="dropdown-item" href="#s03">Exercice 03</a></li>
+        <li><a class="dropdown-item" href="#s04">Exercice 04</a></li>
+        <li><a class="dropdown-item" href="#s05">Exercice 05</a></li>
+        <li><a class="dropdown-item" href="#s06">Exercice 06</a></li>
+        <li><a class="dropdown-item" href="#s07">Exercice 07</a></li>
+        <li><a class="dropdown-item" href="#s08">Exercice 08</a></li>
+        <li><a class="dropdown-item" href="#s09">Exercice 09</a></li>
+    </ul>
+</div>
+
+<!-- Saison CRUD -->
+<div class="btn-group">
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+    TP CRUD</button>
+    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <li><a class="dropdown-item" href="#s01">Exercice 01</a></li>
+    </ul>
+</div>
