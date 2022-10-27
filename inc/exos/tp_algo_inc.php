6df5f07273b2718f7fc095e345c770fc8080554f
Lesort-Pajot Sebastien <sebastien@lesort-pajot.com>
2022-10-27 18:36:35 +0000

    2022-10-27--18-36

--- inc/exos/tp_algo_inc.php
+++ inc/exos/tp_algo_inc.php
@@ -1,3 +1,12 @@
+<div class="row">
+    <div class="nav nav-tabs" id="nav-tab" role="tablist">
+    <a class="nav-item nav-link active" id="nav-ennonce-tab" data-toggle="tab" href="#nav-ennonce" role="tab" aria-controls="nav-ennonce" aria-selected="true">Ennoncé</a>
+    <a class="nav-item nav-link" id="nav-pseudo-tab" data-toggle="tab" href="#nav-pseudo" role="tab" aria-controls="nav-pseudo" aria-selected="false">PseudoCode</a>
+    <a class="nav-item nav-link" id="nav-js-tab" data-toggle="tab" href="#nav-js" role="tab" aria-controls="nav-js" aria-selected="false">JS</a>
+    <a class="nav-item nav-link" id="nav-jq-tab" data-toggle="tab" href="#nav-jq" role="tab" aria-controls="nav-jq" aria-selected="false">JQ</a>
+    <a class="nav-item nav-link" id="nav-php-tab" data-toggle="tab" href="#nav-php" role="tab" aria-controls="nav-php" aria-selected="false">PHP</a>
+</div>
+
 <?php
     echo "<H3>TP Algo - Saison ". $saison . " - Exercice " . $exercice . "</H3>";
     $txtPRINCIPALennonce = file_get_contents('inc/exos/s_' . $saison . '/exo_' . $exercice . '/exoENNONCE.txt');
@@ -7,19 +16,34 @@
     $txtPRINCIPALphp = file_get_contents('inc/exos/s_' . $saison . '/exo_' . $exercice . '/exoPHP.php'); 
 ?>
 
-<div class="tab-content" id="myTabContent">
-    <div class="tab-pane fade show active" id="ennonce" role="tabpanel">
+<div class="tab-content" id="nav-tabContent">
+    <div class="tab-pane fade show active" id="nav-ennonce" role="tabpanel" aria-labelledby="nav-ennonce-tab">
         <?php
             echo '<p><textarea id="ENNONCE" name="ENNONCE" rows="6" cols="120">' . $txtPRINCIPALennonce . '</textarea></p>'; 
-        ?>        
+        ?>      
     </div>
-</div>
-
-<div class="tab-content" id="myTabContent">
-    <div class="tab-pane fade show active" id="pseudo" role="tabpanel">
+    
+    <div class="tab-pane fade" id="nav-pseudo" role="tabpanel"  aria-labelledby="nav-pseudo-tab">
         <?php
             echo '<p><textarea id="PSEUDO" name="PSEUDO" rows="6" cols="120">' . $txtPRINCIPALpseudo . '</textarea></p>'; 
-        ?>        
+        ?>
+    </div>
+    
+    <div class="tab-pane fade" id="nav-js" role="tabpanel" aria-labelledby="nav-js-tab">
+        <?php
+            echo '<p><textarea id="JS" name="JS" rows="6" cols="120">' . $txtPRINCIPALjs . '</textarea></p>'; 
+        ?>            
+    </div>
+
+    <div class="tab-pane fade" id="nav-jq" role="tabpanel" aria-labelledby="nav-jq-tab">
+        <?php
+            echo '<p><textarea id="JQ" name="JQ" rows="6" cols="120">' . $txtPRINCIPALjq . '</textarea></p>'; 
+        ?>             
+    </div>
+
+    <div class="tab-pane fade" id="nav-php" role="tabpanel" aria-labelledby="nav-php-tab">
+        <?php
+            echo '<p><textarea id="PHP" name="PHP" rows="6" cols="120">' . $txtPRINCIPALphp . '</textarea></p>'; 
+        ?>    
     </div>
 </div>
-
--- inc/header_inc.php
+++ inc/header_inc.php
@@ -1,49 +1,30 @@
 <div class="container">
-    <div class="row">
-        <ul class="nav nav-pills nav-justified" id="myTab" role="tablist">
-            <li class="nav-item">
-                <a class="nav-link active" id="ennonce-tab" data-toggle="tab" href="#ennonce" role="tab" aria-controls="home" aria-selected="true">Ennonce</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" id="pseudo-tab" data-toggle="tab" href="#pseudo" role="tab" aria-controls="profile" aria-selected="false">Pseudo</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" id="js-tab" data-toggle="tab" href="#js" role="tab" aria-controls="contact" aria-selected="false">JS</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" id="jq-tab" data-toggle="tab" href="#jq" role="tab" aria-controls="contact" aria-selected="false">JQ</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" id="php-tab" data-toggle="tab" href="#php" role="tab" aria-controls="contact" aria-selected="false">PHP</a>
-            </li>
-        </ul>
-        
-        <?php
-            $saison = $_GET['s'];
-            $exercice = $_GET['e'];
-            if ($saison == null or $exercice == null){
-                $saison = "01";
-            }
-            if ($exercice == null){
-                $exercice = "01";
-            }
-            $saisonFR= str_replace("s_" , "Saison " , $saison);
-            $exerciceFR = str_replace("exo_" , "Exercice " , $exercice);
-            // =============================== TRY to identify exercice ==============================================
-            switch ($exercice) {
-                // TP CRUD found
-                case 'tp_crud':
-                $txtPRINCIPALdatatables = file_get_contents('inc/exos/s_' . $saison . '/exo_' . $exercice . '/exoPHP.php');
+    <?php
+        $saison = $_GET['s'];
+        $exercice = $_GET['e'];
+        if ($saison == null or $exercice == null){
+            $saison = "01";
+        }
+        if ($exercice == null){
+            $exercice = "01";
+        }
+        $saisonFR= str_replace("s_" , "Saison " , $saison);
+        $exerciceFR = str_replace("exo_" , "Exercice " , $exercice);
+        // =============================== TRY to identify exercice ==============================================
+        switch ($exercice) {
+            // TP CRUD found
+            case 'tp_crud':
+            $txtPRINCIPALdatatables = file_get_contents('inc/exos/s_' . $saison . '/exo_' . $exercice . '/exoPHP.php');
+            break;
+            // TP CV found
+            case 'tp_cv':
+                $txtPRINCIPALphp = file_get_contents('inc/exos/s_' . $saison . '/exo_' . $exercice . '/exoPHP.php');
                 break;
-                // TP CV found
-                case 'tp_cv':
-                    $txtPRINCIPALphp = file_get_contents('inc/exos/s_' . $saison . '/exo_' . $exercice . '/exoPHP.php');
-                    break;
-                // Exercice NOT FOUND in all cases then display default EXERCICE FORM (TP_ALGO)
-                default:
-                    require('inc/exos/tp_algo_inc.php');
-                }
-            // =============================== End of TRY to identify exercice ==============================================
-        ?>
+            // Exercice NOT FOUND in all cases then display default EXERCICE FORM (TP_ALGO)
+            default:
+                require('inc/exos/tp_algo_inc.php');
+            }
+        // =============================== End of TRY to identify exercice ==============================================
+    ?>
     </div>
 </div>
