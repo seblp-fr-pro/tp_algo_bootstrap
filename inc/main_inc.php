e7375a88b304563865a8d37ccf63063ed77700ac
Lesort-Pajot Sebastien <sebastien@lesort-pajot.com>
2022-10-27 11:24:00 +0000

    2022-10-27--11-23

--- inc/main_inc.php
+++ inc/main_inc.php
@@ -1,102 +1,59 @@
 <div class="tab-content">
+
     <div id="s01" class="tab-pane fade in active"> 
         <h3>Saison 01</h3>
-            <!-- NAV menu  -->
-            <div class="container">
-            <div class="row">
-                <?php require('inc/exos/s_01/exo_01.php');?>
-            </div>
-        </div>
-    </div>  
+        <?php require('inc/exos/s_01/exos_inc.php');?>
+    </div>
 
     <div id="s02" class="tab-pane fade in"> 
         <h3>Saison 02</h3>
-            <!-- NAV menu  -->
-            <div class="container">
-            <div class="row">
-                <?php require('inc/exos/s_01/exo_01.php');?>
-            </div>
-        </div>
-    </div>  
+        <?php require('inc/exos/s_02/exos_inc.php');?>
+    </div>
 
     <div id="s03" class="tab-pane fade in"> 
         <h3>Saison 03</h3>
-            <!-- NAV menu  -->
-            <div class="container">
-            <div class="row">
-                <?php require('inc/exos/s_01/exo_01.php');?>
-            </div>
-        </div>
-    </div>  
+        <?php require('inc/exos/s_03/exos_inc.php');?>
+    </div>
 
     <div id="s04" class="tab-pane fade in"> 
         <h3>Saison 04</h3>
-            <!-- NAV menu  -->
-            <div class="container">
-            <div class="row">
-                <?php require('inc/exos/s_01/exo_01.php');?>
-            </div>
-        </div>
-    </div>  
+        <?php require('inc/exos/s_04/exos_inc.php');?>
+    </div>
 
     <div id="s05" class="tab-pane fade in"> 
         <h3>Saison 05</h3>
-            <!-- NAV menu  -->
-            <div class="container">
-            <div class="row">
-                <?php require('inc/exos/s_01/exo_01.php');?>
-            </div>
-        </div>
-    </div>  
+        <?php require('inc/exos/s_05/exos_inc.php');?>
+    </div>
 
     <div id="s06" class="tab-pane fade in"> 
         <h3>Saison 06</h3>
-            <!-- NAV menu  -->
-            <div class="container">
-            <div class="row">
-                <?php require('inc/exos/s_01/exo_01.php');?>
-            </div>
-        </div>
-    </div>  
+        <?php require('inc/exos/s_06/exos_inc.php');?>
+    </div>
 
     <div id="s07" class="tab-pane fade in"> 
         <h3>Saison 07</h3>
-            <!-- NAV menu  -->
-            <div class="container">
-            <div class="row">
-                <?php require('inc/exos/s_01/exo_01.php');?>
-            </div>
-        </div>
-    </div>  
+        <?php require('inc/exos/s_07/exos_inc.php');?>
+    </div>
 
     <div id="s08" class="tab-pane fade in"> 
         <h3>Saison 08</h3>
-            <!-- NAV menu  -->
-            <div class="container">
-            <div class="row">
-                <?php require('inc/exos/s_01/exo_01.php');?>
-            </div>
-        </div>
-    </div>  
+        <?php require('inc/exos/s_08/exos_inc.php');?>
+    </div>
 
     <div id="s09" class="tab-pane fade in"> 
         <h3>Saison 09</h3>
-            <!-- NAV menu  -->
-            <div class="container">
-            <div class="row">
-                <?php require('inc/exos/s_01/exo_01.php');?>
-            </div>
-        </div>
-    </div>  
+        <?php require('inc/exos/s_09/exos_inc.php');?>
+    </div>
 
     <div id="s10" class="tab-pane fade in"> 
         <h3>Saison 10</h3>
-            <!-- NAV menu  -->
-            <div class="container">
-            <div class="row">
-                <?php require('inc/exos/s_01/exo_01.php');?>
-            </div>
-        </div>
-    </div>  
+        <?php require('inc/exos/s_10/exos_inc.php');?>
+    </div>
+
+    <div id="sCRUD" class="tab-pane fade in"> 
+        <h3>Saison TP CRUD</h3>
+        <?php require('inc/exos/crud/exos_inc.php');?>
+    </div>
+
 
 </div>
--- index.php
+++ index.php
@@ -6,36 +6,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!-- CSS scripts  -->
+    <!-- CSS STYLE scripts  -->
         <?php require('inc/css_inc.php');?>
 
     <title>TP Algo / Bootstrap / CRUD</title>
 </head>
 
 <body>
-    <!-- NAV menu  -->
-        <div class="container">
-            <div class="row">
-                <?php require('inc/nav_inc.php');?>
-            </div>
-        </div>
 
-    <!-- MAIN container  -->
-        <div class="container">
-            <div class="row">
-                <?php require('inc/main_inc.php');?>
-            </div>
-        </div>
+    <!-- NAV MENU container used by this website -->
+        <?php require('inc/nav_inc.php');?>
 
-    <!-- FOOTER container  -->
-        <div class="container">
-            <div class="row">
-                <?php require('inc/footer_inc.php');?>
-            </div>
-        </div>
+    <!-- MAIN container used by this website -->
+        <?php require('inc/main_inc.php');?>
+
+    <!-- FOOTER container used by this website -->
+        <?php require('inc/footer_inc.php');?>
 
     <!-- JS SCRIPTS used by this website  -->
         <?php require('inc/scripts_inc.php');?>
+
 </body>
 
 </html>
