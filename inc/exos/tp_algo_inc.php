908ddc7a3fc34ccf7740834c44c01fe58ed8bec6
Lesort-Pajot Sebastien <sebastien@lesort-pajot.com>
2022-10-31 17:01:12 +0000

    try to merge and make dynamic code with PHP 2

--- inc/exos/tp_algo_inc.php
+++ inc/exos/tp_algo_inc.php
@@ -1,15 +1,17 @@
 <div class="row">
     <div class="nav nav-tabs" id="nav-tab" role="tablist">
         <?php
+            // GET tab number 
+                $number_tab = $_GET['tab'];
+
             // CHANGE title on HTML page with dynamic data
                 echo "<title>TP Algo - S". $saison . "-Ex" . $exercice . "</title>";
 
             // ARRAY with differents tab availables
-                $tab_list = array("ennonce", "pseudo", "js", "jq", "php");
+                $tab_list = array("Consignes", "PseudoCode", "JS", "JQ", "PHP");
             
             // LOOP with all tabs
-                foreach ($tab_list as $tab_name) 
-                {
+                foreach ($tab_list as $tab_name){
                     // UPPERCASE $tab_name as $tab_name_CAP
                         $tab_name_CAP = strtoupper($tab_name);
 
@@ -17,6 +19,13 @@
                         // Exo
                             ${"txt$tab_name_CAP"} = file_get_contents('inc/exos/s_' . $saison . '/exo_' . $exercice . "/exo${"txt$tab_name_CAP"}.txt");
 
+                            echo <<< _END
+                            <a class="nav-item nav-link" id="nav-$tab_name-tab" data-toggle="tab" href="#nav-$tab_name" role="tab" aria-controls="nav-$tab_name" aria-selected="false">$tab_name</a>
+                            _END;
+
+                }
+
+                foreach ($tab_list as $tab_name){
                     // Display tab content
                         echo <<<_END
                             <form action="?s=$saison&e=$exercice&tab=1#nav-$tab_name" method="post">
@@ -28,29 +37,23 @@
                         _END;
 
                     // WRITE functions to write text on txtfiles
-                        function writeENNONCE(){
-                            $fname=$txtENNONCE;
-                            $fh=fopen($fname,'w');
-                            $data=$_POST['texteENNONCE'];
+                        function write($tab_name_CAP, $data_exo){
+                            $file_name=${txt_};
+                            $fh=fopen($file_name,'w');
+                            $data=$_POST["texte$tab_name_CAP"];
                             fwrite($fh,$data);
                             fclose($fh);
                         }
 
                     // Method POST with differents forms
-                        if(isset($_POST['submitENNONCE'])){writeENNONCE();}
+                        if(isset($_POST["submit$tab_name_CAP"])){write($tab_name_CAP);}
                 }
 
 
-            $number_tab = $_GET['tab'];
+
             switch($number_tab) {      
                 case 2:
-                    echo <<< _END
-                        <a class="nav-item nav-link" id="nav-ennonce-tab" data-toggle="tab" href="#nav-ennonce" role="tab" aria-controls="nav-ennonce" aria-selected="false">Ennoncé</a>
-                        <a class="nav-item nav-link active" id="nav-pseudo-tab" data-toggle="tab" href="#nav-pseudo" role="tab" aria-controls="nav-pseudo" aria-selected="true">PseudoCode</a>    
-                        <a class="nav-item nav-link" id="nav-js-tab" data-toggle="tab" href="#nav-js" role="tab" aria-controls="nav-js" aria-selected="false">JS</a>
-                        <a class="nav-item nav-link" id="nav-jq-tab" data-toggle="tab" href="#nav-jq" role="tab" aria-controls="nav-jq" aria-selected="false">JQ</a>
-                        <a class="nav-item nav-link" id="nav-php-tab" data-toggle="tab" href="#nav-php" role="tab" aria-controls="nav-php" aria-selected="false">PHP</a>
-                    _END;
+
                 break;
                 
                 default:
