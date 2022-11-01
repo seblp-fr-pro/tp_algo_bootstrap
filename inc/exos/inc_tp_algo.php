97902042c1734e8fdbf705b86209d65f77048805
Lesort-Pajot Sebastien <sebastien@lesort-pajot.com>
2022-11-01 16:23:14 +0000

    rename and optimisation try to unbug 5

--- inc/exos/inc_tp_algo.php
+++ inc/exos/inc_tp_algo.php
@@ -1,8 +1,18 @@
 <div class="row">
     <div class="nav nav-tabs" id="nav-tab" role="tablist">
         <?php
+            // Fonctions used by this script
+                // WRITE functions to write text on txtfiles
+                function write($tab_name_CAP) {
+                    $file_name=$exo_file_name;
+                    $fh=fopen($file_name,'w');
+                    $data=$_POST[${"txt$tab_name_CAP"}];
+                    fwrite($fh,$data);
+                    fclose($fh);
+                }
+
             // Folders
-                 $tp_algo_folder='inc/exos/s_' . $saison . '/exo_' . $exercice . '/';
+                $tp_algo_folder='inc/exos/s_' . $saison . '/exo_' . $exercice . '/';
 
             // GET tab number 
                 $number_tab = $_GET['tab'];
@@ -16,25 +26,25 @@
             // LOOP with all tabs
                 foreach ($tab_list as $tab_name){
 
-                    // Precise file used by this scipt
+                    // Precise location files used by this scipt
                         switch ($tab_name) {
-                            case "Consignes":
+                            case 'Consignes':
                                 $exo_file_name = $tp_algo_folder . 'exoCONSIGNES.txt';
                             break;
 
-                            case "PseudoCode":
+                            case 'PseudoCode':
                                 $exo_file_name = $tp_algo_folder . 'exoPSEUDOCODE.txt';
                             break;
 
-                            case "JS":
+                            case 'JS':
                                 $exo_file_name = $tp_algo_folder . 'exoJS.js';
                             break;
 
-                            case "JQ":
+                            case 'JQ':
                                 $exo_file_name = $tp_algo_folder . 'exoJQ.js';
                             break;
 
-                            case "PHP":
+                            case 'PHP':
                                 $exo_file_name = $tp_algo_folder . 'exoPHP.php';
                             break;
                         }
@@ -44,42 +54,36 @@
 
                     // DISPLAY text in files inc/exos/s_xx/exo-xx/exoXXXX.XXX
                         // DISPLAY TABS names
-                            ${"txt$tab_name_CAP"} = file_get_contents($exo_file_name);
+                            $displayME = file_get_contents($exo_file_name);
 
                             echo <<< _END
                                 <a class="nav-item nav-link" id="nav-$tab_name-tab" data-toggle="tab" href="#nav-$tab_name" role="tab" aria-controls="nav-$tab_name" aria-selected="false">$tab_name</a>
                             _END;
-                }
 
-                foreach ($tab_list as $tab_name){
                     // Display each TAB content
-                        echo <<<_END
-                            <div class="row">
-                                <div class="tab-content" id="nav-tabContent">
-                                    <div class="tab-pane fade show active" id="nav-$tab_name" role="tabpanel" aria-labelledby="nav-$tab_name-tab">
-                                        <form action="?s=$saison&e=$exercice&tab=1#nav-$tab_name" method="post">
-                                            <p><pre id="editor$tab_name_CAP" class="myeditor" name="texte$tab_name_CAP">${"txt$tab_name_CAP"}</pre></p>
+                        echo <<<_END2
+                            <div class="tab-content" id="nav-tabContent">
+                                <div class="row align-items-start">
+                                    <div class="col-1">
+                                        <div tab-pane fade show" id="nav-$tab_name" role="tabpanel" aria-labelledby="nav-$tab_name-tab">
+                                            <form action="?s=$saison&e=$exercice&tab=1#nav-$tab_name" method="post">
+                                                <p><pre id="editor$tab_name_CAP" class="myeditor" name="texte$tab_name_CAP">$displayME</pre></p>
+                                            </form>
+                                        </div>
 
+                                        <div class="col-2">
                                             <button type="submit" name="submit$tab_name_CAP" class="btn btn-danger">Enregistrer $tab_name_CAP</button>
                                             <button type="button" class="btn btn-success">Exécuter $tab_name_CAP</button>
-                                        </form>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        _END;
-
-                    // WRITE functions to write text on txtfiles
-                        function write($tab_name_CAP, $data_exo){
-                            $file_name=${txt_};
-                            $fh=fopen($file_name,'w');
-                            $data=$_POST["texte$tab_name_CAP"];
-                            fwrite($fh,$data);
-                            fclose($fh);
-                        }
+                        _END2;
 
                     // Method POST with differents forms
                         if(isset($_POST["submit$tab_name_CAP"])){write($tab_name_CAP);}
                 }
+
         ?>
     </div>
 </div>
